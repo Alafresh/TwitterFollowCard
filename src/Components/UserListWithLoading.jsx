b7d890f9466fa99d20292d7a47afec1a208a6f33
juan_ <alafresh16@gmail.com>
2025-04-12 15:48:19 -0500

    manejo de errores y cargar dependencias

--- src/Components/UserListWithLoading.jsx
+++ src/Components/UserListWithLoading.jsx
@@ -1,13 +1,35 @@
 import { useState, useEffect } from 'react';
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error('Error fetching users:', error));
+    const fecthUsers = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok' + response.statusText);
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fecthUsers();
   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
   return (
     <div>
